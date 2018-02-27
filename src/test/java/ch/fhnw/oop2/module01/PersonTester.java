2f8d5653e0489311b91cfb0f79e658660eeedcde
Nguyen <chicuong.nguyen@students.fhnw.ch>
2018-02-27 12:14:19 +0100

    first Junit-Test finished

--- src/test/java/ch/fhnw/oop2/module01/PersonTester.java
+++ src/test/java/ch/fhnw/oop2/module01/PersonTester.java
@@ -7,10 +7,11 @@ public class PersonTester {
 
     @Test
     void bmiTest(){
+        //given
         Person p = new Person(23,65,1.78);
-
+        //when
         double bmi = p.getBMI();
-
+        //then
         Assertions.assertEquals(20.5, bmi, 0.1);
     }
 }
