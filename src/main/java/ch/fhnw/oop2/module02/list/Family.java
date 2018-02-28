5ee55110caa7d1099b4087bab6484928d284ebe0
Nguyen <chicuong.nguyen@students.fhnw.ch>
2018-02-28 07:09:19 +0100

    module02 startpunkt

--- src/main/java/ch/fhnw/oop2/module02/list/Family.java
+++ src/main/java/ch/fhnw/oop2/module02/list/Family.java
@@ -1,10 +1,40 @@
 package ch.fhnw.oop2.module02.list;
 
+import ch.fhnw.oop2.module01.Person;
+
+import java.util.ArrayList;
+
 /**
  * @author Dieter Holz
  */
 public class Family {
 
+    private ArrayList<Person> members = new ArrayList<>();
 
+    public Family(){members = new ArrayList<>();}
+    public int size(){
+        return members.size();
+    }
 
+    public void add(Person p){
+        if(isMember(p)){
+            return;
+        }
+        members.add(p);
+    }
+
+    public boolean isMember(Person p){
+        return members.contains(p);
+    }
+
+    public Person getOldest(){
+        Person oldest = members.get(0);
+        for(Person p : members){
+            if(p.getAge() > oldest.getAge()){
+                oldest = p;
+            }
+        }
+        return oldest;
+
+    }
 }
